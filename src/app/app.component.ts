23be0ade7ddeb89ae6a23889fb0d9ca27604ae86
goutham_nithyananda <goutham.nithyananda@oracle.com>
2020-06-07 13:19:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angula8-table-pagination';
+  employees=[{
+    "name":"John",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Ganesh",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Larry",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Rama",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Ravi",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Suresh",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Babu",
+    "city":"Bengaluru"
+  },{
+    "name":"Ajay",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Deepak",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Prashanth",
+    "city":"Bengaluru"
+  },{
+    "name":"Nikhil",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Sushma",
+    "city":"Bengaluru"
+  },
+  {
+    "name":"Sowmya",
+    "city":"Bengaluru"
+  }];
+  
+
+  config = {
+    itemsPerPage: 3,
+    currentPage: 1,
+    totalItems: this.employees.length
+  };
+
+  pageChanged(event) {
+    this.config.currentPage = event;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
